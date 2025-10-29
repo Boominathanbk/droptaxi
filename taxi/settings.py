c98d7f077c1c20be555658d225ce3d5ea82bd0a9
Boominathan <boominathanpoongavanam@gmail.com>
2025-10-29 22:38:03 +0530

    Use environment variables for email configuration

--- taxi/settings.py
+++ taxi/settings.py
@@ -154,8 +154,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+
+
+print("EMAIL HOST USER:", EMAIL_HOST_USER)
+print("EMAIL HOST PASSWORD:", EMAIL_HOST_PASSWORD is not None)
