d2966aeb4226380d9a84ef234fb39287a88ffac9
Boominathanbk <123536895+Boominathanbk@users.noreply.github.com>
2026-01-01 22:24:37 +0530

    Update sitemaps.py

--- app/sitemaps.py
+++ app/sitemaps.py
@@ -1,1 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 
+class StaticViewSitemap(Sitemap):
+    priority = 0.5          # importance
+    changefreq = 'monthly'  # frequency
+
+    def items(self):
+        # URL pattern names from urls.py
+        return ['homepage', 'about', 'booking', 'round', 'round_booking']
+
+    def location(self, item):
+        return reverse(item)
