29c7c0b7ca1b105de656897fcbae8d5d8d997674
Boominathanbk <123536895+Boominathanbk@users.noreply.github.com>
2026-01-01 22:26:25 +0530

    Update urls.py

--- taxi/urls.py
+++ taxi/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django-sitemap'),
 ]
