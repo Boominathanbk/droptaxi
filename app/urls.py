7e3a8ce7939ed8941f6cc3c9b613e12d0aeba3e1
Boominathanbk <123536895+Boominathanbk@users.noreply.github.com>
2026-02-21 23:32:16 +0530

    Add new path for enquiries in urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -11,5 +12,6 @@ urlpatterns = [
     path('round_booking',views.round_booking, name='round_booking'),
     path('roundtrip',views.roundtrip, name='roundtrip'),
     path('about',views.about, name='about'),
+    path('enquries/', views.enquries, name='enquries'),
 
 ]
