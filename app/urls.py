97f4721e546907f241d11e491f34d615cdce51bc
Boominathanbk <123536895+Boominathanbk@users.noreply.github.com>
2026-03-17 21:11:50 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,16 @@
-
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-urlpatterns = [
-    path('',views.homepage,name='homepage'),
-    path('round',views.round, name='round'),
-    path('booking',views.booking, name='booking'),
-    path('enqurie/', views.enqurie, name='enqurie'),
-    path('login',views.login,name='login'),
-    path('login_data',views.login_data,name='login_data'),
-    path('round_booking',views.round_booking, name='round_booking'),
-    path('roundtrip',views.roundtrip, name='roundtrip'),
-    path('about',views.about, name='about'),
-    path('enquries/', views.enquries, name='enquries'),
 
+
+urlpatterns = [
+   path('',views.homepage,name='homepage'),
+   path('round',views.round, name='round'),
+   path('booking',views.booking,name='booking'),
+   path('round_booking',views.round_booking, name='round_booking'),
+   
+   path('enqurie/', views.enqurie, name='enqurie'), 
+   path('enquries/', views.enquries, name='enquries'),
+   path('terms', views.terms, name='terms'),
 ]
+
